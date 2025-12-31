9781b4d03f4544ae82d19ec791dea6b63afd1f15
Bobby Evans <bobbyevans@Bobbys-iMac.local>
2025-12-31 16:08:17 -0600

    Revert "Fix email template column name"
    This reverts commit 2ad76f6c413e6b3c649fe1ed3b30027d70865b14.

--- src/app/admin/winners/page.js
+++ src/app/admin/winners/page.js
@@ -529,7 +529,7 @@ export default function AdminWinnersPage() {
             // Fetch the prize_winner email template
             const { data: template, error } = await supabase
                 .from('email_templates')
-                .select('subject, html_body')
+                .select('subject, html_content')
                 .eq('template_key', 'prize_winner')
                 .single()
 
@@ -547,14 +547,7 @@ export default function AdminWinnersPage() {
                 .replace(/\{\{username\}\}/g, verificationData.user.username)
                 .replace(/\{\{prize_name\}\}/g, prizeDisplay)
 
-            let body = template.html_body
-                ```
-
----
-
-save, then in **terminal**:
-```
-cd ~/desktop/creative - advertising - platform && git add. && git commit - m "fix email template column name" && git push
+            let body = template.html_content
                 .replace(/\{\{username\}\}/g, verificationData.user.username)
                 .replace(/\{\{prize_name\}\}/g, prizeDisplay)
                 .replace(/\{\{game_type\}\}/g, 'Slots Drawing')
